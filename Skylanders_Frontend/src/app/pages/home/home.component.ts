b8db5dd371c3434d795df95a9f4eca5096982403
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-15 21:24:05 +0200

    home creado

--- Skylanders_Frontend/src/app/pages/home/home.component.ts
+++ Skylanders_Frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../componentes/header/header.component";
+import { PresentacionComponent } from "../../componentes/presentacion/presentacion.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, PresentacionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
