ab0654c087e51a9f97dc887c000636b05f740ce4
guillermooom <97970403+guillermooom@users.noreply.github.com>
2024-07-17 19:13:39 +0200

    saqgas con error

--- Skylanders_Frontend/src/app/pages/saga-detalles/saga-detalles.component.ts
+++ Skylanders_Frontend/src/app/pages/saga-detalles/saga-detalles.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GiantsComponent } from "../../componentes/sagas/giants/giants.component";
 import { HeaderComponent } from "../../componentes/header/header.component";
+import { NotFoundComponent } from "../not-found/not-found.component";
 
 @Component({
   selector: 'app-saga-detalles',
   standalone: true,
-  imports: [GiantsComponent, HeaderComponent],
+  imports: [GiantsComponent, HeaderComponent, NotFoundComponent],
   templateUrl: './saga-detalles.component.html',
   styleUrl: './saga-detalles.component.css'
 })
@@ -24,6 +25,10 @@ export class SagaDetallesComponent implements OnInit {
 
   volverSagas(){
     this.router.navigate(['sagas']);
+  }
+
+  irError(){
+    this.router.navigate(['not_found']);
   }
 
 }
